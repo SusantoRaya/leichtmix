6871ba7765dd8292aa5e90604f263f030aace328
susanto raya <stox.raya@gmail.com>
2025-10-21 22:32:38 +0700

    fix spacing di mobile

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -8,104 +8,102 @@ $this->title = 'Leichtmix Premium Mortar - Bersama Membangun Tanah Air';
 <!-- youtube video  -->
 
 
-<div class="container-fluid">
-    <!-- Start video Area -->
-    <div class="row uniq__banner__area">
-        <!-- Start video -->
-        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-            <div class="banner style--2">
-                <div class="video-container">
+<!-- <div class="container-fluid"> -->
+<!-- Start video Area -->
+<div class="row uniq__banner__area">
+    <!-- Start video -->
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12  pb--10">
+        <div class="banner style--2">
+            <div class="video-container">
 
-                    <iframe class="video-background"
-                        src="https://www.youtube.com/embed/1nRBH_SHu8o?autoplay=1&controls=1&loop=1&playlist=1nRBH_SHu8o"
-                        frameborder="0"
-                        allow="autoplay; encrypted-media"
-                        allowfullscreen>
-                    </iframe>
-                </div>
-
+                <iframe class="video-background"
+                    src="https://www.youtube.com/embed/1nRBH_SHu8o?autoplay=1&controls=1&loop=1&playlist=1nRBH_SHu8o"
+                    frameborder="0"
+                    allow="autoplay; encrypted-media"
+                    allowfullscreen>
+                </iframe>
             </div>
-        </div>
 
+        </div>
     </div>
-    <!-- End Banner Area -->
+
 </div>
+<!-- End Banner Area -->
+<!-- </div> -->
 
 <!-- showcase  -->
-<div class="container-fluid pt--10">
-    <!-- Start Banner Area -->
-    <div class="row uniq__banner__area">
-        <!-- Start Single Banner -->
-        <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 ">
-            <?php $banner = Yii::$app->banner->getByPage('homecat1'); ?>
-            <?php if ($banner): ?>
-                <div class="banner style--2">
-                    <div class="thumb">
-                        <a href="#"><img src="<?= $banner->getImageUrl() ?>" alt="<?= $banner->title ?>"></a>
-                    </div>
-                    <div class="content">
-                        <h6 class="text-uppercase"><?= $banner->title ?></h6>
-                        <a class="htc__btn shop__now__btn btn btn-outline-light" href="<?= $banner->link ?>">Pelajari Lebih Lanjut</a>
-                    </div>
+<!-- <div class="container-fluid pt--10"> -->
+<!-- Start Banner Area -->
+<div class="row uniq__banner__area">
+    <!-- Start Single Banner -->
+    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 pb--10">
+        <?php $banner = Yii::$app->banner->getByPage('homecat1'); ?>
+        <?php if ($banner): ?>
+            <div class="banner style--2">
+                <div class="thumb">
+                    <a href="#"><img src="<?= $banner->getImageUrl() ?>" alt="<?= $banner->title ?>"></a>
                 </div>
-            <?php endif; ?>
-        </div>
-        <!-- Start Single Banner -->
-        <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 smt-40 xmt-40">
-            <?php $banner = Yii::$app->banner->getByPage('homecat2'); ?>
-            <?php if ($banner): ?>
-                <div class="banner style--2">
-                    <div class="thumb">
-                        <a href="#"><img src="<?= $banner->getImageUrl() ?>" alt="<?= $banner->title ?>"></a>
-                    </div>
-                    <div class="content">
-                        <h6 class="text-uppercase"><?= $banner->title ?></h6>
-                        <a class="htc__btn shop__now__btn btn btn-outline-light" href="<?= $banner->link ?>">Pelajari Lebih Lanjut</a>
-                    </div>
+                <div class="content">
+                    <h6 class="text-uppercase"><?= $banner->title ?></h6>
+                    <a class="htc__btn shop__now__btn btn btn-outline-light" href="<?= $banner->link ?>">Pelajari Lebih Lanjut</a>
                 </div>
-            <?php endif; ?>
-        </div>
-
-        <div class="clearfix"></div>
-
-        <!-- Start Single Banner -->
-        <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 smt-40 xmt-40 pt--20">
-            <?php $banner = Yii::$app->banner->getByPage('homecat3'); ?>
-            <?php if ($banner): ?>
-                <div class="banner style--2">
-                    <div class="thumb">
-                        <a href="#"><img src="<?= $banner->getImageUrl() ?>" alt="<?= $banner->title ?>"></a>
-                    </div>
-                    <div class="content">
-                        <h6 class="text-uppercase"><?= $banner->title ?></h6>
-                        <a class="htc__btn shop__now__btn btn btn-outline-light" href="<?= $banner->link ?>">Pelajari Lebih Lanjut</a>
-                    </div>
+            </div>
+        <?php endif; ?>
+    </div>
+    <!-- Start Single Banner -->
+    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 pb--10">
+        <?php $banner = Yii::$app->banner->getByPage('homecat2'); ?>
+        <?php if ($banner): ?>
+            <div class="banner style--2">
+                <div class="thumb">
+                    <a href="#"><img src="<?= $banner->getImageUrl() ?>" alt="<?= $banner->title ?>"></a>
                 </div>
-            <?php endif; ?>
-        </div>
-
-        <!-- Start Single Banner -->
-        <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 smt-40 xmt-40 pt--20">
-            <?php $banner = Yii::$app->banner->getByPage('homecat4'); ?>
-            <?php if ($banner): ?>
-                <div class="banner style--2">
-                    <div class="thumb">
-                        <a href="#"><img src="<?= $banner->getImageUrl() ?>" alt="<?= $banner->title ?>"></a>
-                    </div>
-                    <div class="content">
-                        <h6 class="text-uppercase"><?= $banner->title ?></h6>
-                        <a class="htc__btn shop__now__btn btn btn-outline-light" href="<?= $banner->link ?>">Pelajari Lebih Lanjut</a>
-                    </div>
+                <div class="content">
+                    <h6 class="text-uppercase"><?= $banner->title ?></h6>
+                    <a class="htc__btn shop__now__btn btn btn-outline-light" href="<?= $banner->link ?>">Pelajari Lebih Lanjut</a>
                 </div>
-            <?php endif; ?>
-        </div>
-
-        <div class="clearfix"></div>
-
-
+            </div>
+        <?php endif; ?>
     </div>
 
-    <!-- End Banner Area -->
+    <div class="clearfix"></div>
+
+    <!-- Start Single Banner -->
+    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 pb--10">
+        <?php $banner = Yii::$app->banner->getByPage('homecat3'); ?>
+        <?php if ($banner): ?>
+            <div class="banner style--2">
+                <div class="thumb">
+                    <a href="#"><img src="<?= $banner->getImageUrl() ?>" alt="<?= $banner->title ?>"></a>
+                </div>
+                <div class="content">
+                    <h6 class="text-uppercase"><?= $banner->title ?></h6>
+                    <a class="htc__btn shop__now__btn btn btn-outline-light" href="<?= $banner->link ?>">Pelajari Lebih Lanjut</a>
+                </div>
+            </div>
+        <?php endif; ?>
+    </div>
+
+    <!-- Start Single Banner -->
+    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 pb--10">
+        <?php $banner = Yii::$app->banner->getByPage('homecat4'); ?>
+        <?php if ($banner): ?>
+            <div class="banner style--2">
+                <div class="thumb">
+                    <a href="#"><img src="<?= $banner->getImageUrl() ?>" alt="<?= $banner->title ?>"></a>
+                </div>
+                <div class="content">
+                    <h6 class="text-uppercase"><?= $banner->title ?></h6>
+                    <a class="htc__btn shop__now__btn btn btn-outline-light" href="<?= $banner->link ?>">Pelajari Lebih Lanjut</a>
+                </div>
+            </div>
+        <?php endif; ?>
+    </div>
+
+    <div class="clearfix"></div>
+
+
 </div>
 
-
+<!-- End Banner Area -->
+<!-- </div> -->
